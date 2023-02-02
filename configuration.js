44469c6af69e8e4079f1e8799eb51f7d946dceee
Gergely Csucs <gergelyc@uio.no>
2023-02-02 20:29:31 +0100

    Towards release
    GitHub pages initial, commented configuration file.

--- configuration.js
+++ configuration.js
@@ -1,50 +1,78 @@
-/*
- * HBP configuration
+/**
+ * Demo configuration running from GitHub pages, using remote data
+ * 
+ * URL parameters are stored in <code>args</code>:<br>
+ * * <code>atlas</code>=WHS_SD_Rat_v4_39um<br>
+ * * <code>series</code>=https://data-proxy.ebrains.eu/api/v1/public/buckets/localizoom/!Series/visualign.json<br>
+ * * <code>pyramids</code>=https://data-proxy.ebrains.eu/api/v1/public/buckets/localizoom
+ * @type Object
  */
 const locators = {
+    /**
+     * Provide the link of an actual QuickNII/VisuAlign JSON.
+     * <br>
+     * This demo simply passes the complete URL, no transformation is done
+     * @param {string} series_id <code>series</code> URL parameter
+     * @returns {string} 
+     */
     SeriesLocator: series_id => series_id,
-    DZILocator: () => {
-    },
-    TileLocator: () => {
-    },
+    /**
+     * Provide the link of an actual DZI descriptor.
+     * <br>
+     * This demo uses the <code>pyramids</code> parameter and also makes a shortcut via
+     * assuming that pyramids reside in a <code>section_id</code>.tif folder. Original
+     * extension is removed by <code>transformSeries</code> below.
+     * @param {string} section_id <code>filename</code> field of a section
+     * @returns {string}
+     */
+    DZILocator: section_id =>
+        `${args.pyramids}/${section_id}.tif/${section_id}.dzi`,
+    /**
+     * Provide the link of an actual image tile.
+     * <br>
+     * This demo uses the <code>pyramids</code> parameter and also makes a shortcut via
+     * assuming that pyramids reside in a <code>section_id</code>.tif folder. Original
+     * extension is removed by <code>transformSeries</code> below.
+     * @param {string} section_id <code>filename</code> field of a section
+     * @param {type} level pyramid level
+     * @param {type} x tile coordinates
+     * @param {type} y tile coordinates
+     * @param {type} format <code>Format</code> field (from the DZI descriptor, usually "png" or "jpg")
+     * @returns {string}
+     */
+    TileLocator: (section_id, level, x, y, format) =>
+        `${args.pyramids}/${section_id}.tif/${section_id}_files`+
+        `/${level}/${x}_${y}.${format}`,
+    /**
+     * Provide the link of the atlas descriptor, atlas data is often just next to LocaliZoom,
+     * and appending a .json extension is enough
+     * @param {type} atlas_id <code>atlas</code> URL parameter
+     * @returns {string}
+     */
     AtlasLocator: atlas_id => atlas_id + ".json",
+    /**
+     * Provide the link of the atlas descriptor, atlas data is often just next to LocaliZoom,
+     * and appending a .pack extension is enough
+     * @param {type} atlas_id <code>atlas</code> URL parameter
+     * @returns {string}
+     */
     AtlasVolumeLocator: atlas_id => atlas_id + ".pack"
 };
+/**
+ * Do whatever transformation on the series descriptor to make the work of various locators easier.<br>
+ * This demo strips filenames from their extension, this makes DZILocator and
+ * TileLocator very simple to implement
+ * @param {type} series
+ */
 function transformSeries(series) {
-    /*
-     * convert extensions to .tif except for one protected bucket:
-     */
-    if (args.pyramids !== "buckets/img-eff39c41-6eaa-4d3f-a91f-ef936e793606") {
-        for (const slice of series.slices) {
-            const filename = slice.filename;
-            const pos = filename.lastIndexOf(".");
-            if (pos >= 0) {
-                slice.filename = filename.substring(0, pos) + ".tif";
-            }
-        }
+    for (const slice of series.slices) {
+        slice.filename = slice.filename.substring(0, slice.filename.lastIndexOf("."));
     }
     /*
-     * argument-specific locators
+     * Small hack: the demo series consists of dark images, overlay colors are set to light here.
+     * This could be an URL parameter too, maybe it becomes one in the future.
      */
-    if (args.pyramids.startsWith("buckets/")) {
-        /*
-         * pyramids in collab bucket
-         */
-        locators.DZILocator = section_id =>
-                `https://data-proxy.ebrains.eu/api/v1/${args.pyramids}/${section_id}/` +
-                    `${section_id.substring(0, section_id.lastIndexOf("."))}.dzi`;
-        locators.TileLocator = (section_id, level, x, y, format) =>
-                `https://data-proxy.ebrains.eu/api/v1/${args.pyramids}/${section_id}/` +
-                    `${section_id.substring(0, section_id.lastIndexOf("."))}_files/${level}/${x}_${y}.${format}`;
-    } else {
-        /*
-         * pyramids in legacy image service container
-         */
-        locators.DZILocator = section_id =>
-                `https://object.cscs.ch/v1/AUTH_08c08f9f119744cbbf77e216988da3eb/${args.pyramids}/${section_id}/` +
-                    `${section_id.substring(0, section_id.lastIndexOf("."))}.dzi`;
-        locators.TileLocator = (section_id, level, x, y, format) =>
-                `https://object.cscs.ch/v1/AUTH_08c08f9f119744cbbf77e216988da3eb/${args.pyramids}/${section_id}/` +
-                    `${section_id.substring(0, section_id.lastIndexOf("."))}_files/${level}/${x}_${y}.${format}`;
-    }
+    document.getElementById("outline").value="#FFFFFF";
+    document.getElementById("ancolor").value="#00FFFF";
+    document.getElementById("nlcolor").value="#00FFFF";
 }
